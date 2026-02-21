e6e3c7e02732d9077751fcababea78ac0a74b043
Ísis Valois Sampaio <isisvaloissampaio@gmail.com>
2026-02-21 13:36:31 -0300

    Fix comment formatting in ex022.py

--- MUNDO_1/ex022.py
+++ MUNDO_1/ex022.py
@@ -1,5 +1,5 @@
 """
-22. Crie um programa que leia o nome completo de uma pessoa e mostre:
+Crie um programa que leia o nome completo de uma pessoa e mostre:
 (a) O nome com todas as letras maiúsculas e minúsculas.
 (b) Quantas letras ao todo (sem considerar espaços).
 (c) Quantas letras tem o primeiro nome.
