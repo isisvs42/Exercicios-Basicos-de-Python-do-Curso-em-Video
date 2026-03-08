6628d9ba31289a7e987876cf7264e637d16adb7f
Isis <isisvaloissampaio@gmail.com>
2026-03-08 19:26:11 -0300

    TV feita

--- MUNDO_4/desafio022/desafio022.py
+++ MUNDO_4/desafio022/desafio022.py
@@ -6,8 +6,35 @@ volume e liga/desliga)
 @ = liga/desliga
 < e > = avançar/voltar nos canais
 + e - = aumentar/diminuir volume
+
+0 para sair do programa
 '''
 from modulo import ControleRemoto
+import os
 
 controle = ControleRemoto()
-controle.mostrarTv()
+
+while True:
+    os.system('clear')
+
+    controle.mostrarTv()
+    entrada = input(f'< CH{controle.canalAtual+1} > - VOL{controle.volumeAtual+1} +    ')
+    
+
+    if entrada == '@':
+        controle.ligaDesliga()
+
+    elif entrada == '>':
+        controle.avancarCanal()
+
+    elif entrada == '<':
+        controle.voltarCanal()
+
+    elif entrada == '+':
+        controle.aumentarVolume()
+
+    elif entrada == '-':
+        controle.diminuirVolume()
+
+    elif entrada == '0':
+        break
--- MUNDO_4/desafio022/modulo.py
+++ MUNDO_4/desafio022/modulo.py
@@ -7,26 +7,26 @@ class ControleRemoto():
     def __init__(self):
         self.canais = [True, False, False, False, False]
         self.volume = [True, False, False, False, False]
-        self.canalAtual = 1
-        self.volumeAtual = 1
-        self.tvLigada = True
+        self.canalAtual = 0
+        self.volumeAtual = 0
+        self.tvLigada = False
 
     def mostrarCanais(self):
         canais_str = ""
         for n, canal in enumerate(self.canais):
             if canal:
-                canais_str += f"[yellow]{n+1}[/] "
+                canais_str += f"[white on purple] {n+1} [/]"
             else:
-                canais_str += f"{n+1} "
+                canais_str += f" {n+1} "
         return canais_str
 
     def mostrarVolume(self):
-        volume_str = ""
+        volume_str = "[white on green]"
         for volume in self.volume:
             if volume:
-                volume_str += "[yellow]▮[/]"
+                volume_str += " [/]"
             else:
-                volume_str += "▯"
+                volume_str += " "
         return volume_str
 
     def mostrarTv(self):
@@ -41,6 +41,70 @@ class ControleRemoto():
 
         print(tv)
 
+    def ligaDesliga(self):
+        self.tvLigada = not self.tvLigada
 
-controle = ControleRemoto()
-controle.mostrarTv()
+    def avancarCanal(self):
+        if not self.tvLigada:
+            print('A TV está desligada!')
+            return
+        
+        self.canalAtual = (self.canalAtual + 1) % 5 # quando o estado atual for 4 e avançarmos, teremos o resto de 5 dividido por 5 igual a 0, reiniciando
+
+        for n in range(0, 5):
+            if n == self.canalAtual:
+                self.canais[n] = True
+            
+            else:
+                self.canais[n] = False
+    
+    def voltarCanal(self):
+        if not self.tvLigada:
+            print('A TV está desligada!')
+            return
+        
+        self.canalAtual = (self.canalAtual - 1) % 5
+            # se estiver no canal 0 e voltar: 0 - 1 = -1 / 5 = 0 e resto -1 (ou seja, último item da lista)
+            # a contagem pode ficar de trás para a frente: -1, -2, -3, -4...
+            # quando chegar no -5: -5 / 5 = -1 e resto 0 (ou seja, o primeiro item da lista, que é o -5)
+        
+        for n in range(4, -1, -1):
+            if n == self.canalAtual:
+                self.canais[n] = True
+            
+            else:
+                self.canais[n] = False
+            
+    def aumentarVolume(self): 
+        if not self.tvLigada:
+            print('A TV está desligada!')
+            return
+        
+        if self.volumeAtual == 4:
+            return
+        else:
+            self.volumeAtual +=1
+
+        for n in range(0, 5):
+            if n == self.volumeAtual:
+                self.volume[n] = True
+            
+            else:
+                self.volume[n] = False
+
+    def diminuirVolume(self): 
+        if not self.tvLigada:
+            print('A TV está desligada!')
+            return
+        
+        if self.volumeAtual == 0:
+            return
+        else:
+            self.volumeAtual -=1
+
+        for n in range(4, -1, -1):
+            if n == self.volumeAtual:
+                self.volume[n] = True
+            
+            else:
+                self.volume[n] = False
